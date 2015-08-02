a93a95db65726d6200a2ee0d7164b0d15fc55076
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-02 12:37:09 -0600

    add css layout for calculator

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// add scripts
+
+$(document).on('ready', function() {
+  console.log('sanity check!');
+
+var input = $('input');
+var tipBtn = $('button');
+
+$('button').on('click', function() {
+  console.log(input.innerHTML);
+
+
+});
+
+
+
+
+
+
+});
