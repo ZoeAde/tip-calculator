641540b83a3d03463deaba6b2c2f582df9d2fa90
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-04 14:37:09 -0600

    returning tip amount to answer box

--- js/main.js
+++ js/main.js
@@ -3,18 +3,12 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 
-var input = $('input');
-var tipBtn = $('button');
-
 $('button').on('click', function() {
-  console.log(input.innerHTML);
-
-
+  var input = $('input');
+  var tipBtn = $('button');
+  var amount = input.val();
+  var tip = (amount * 0.2).toFixed(0);
+  $('#answer').text("$" + tip);
 });
-
-
-
-
-
 
 });
